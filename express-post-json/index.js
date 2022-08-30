4af4a8b3dacc97a1c65d15df7b7177ea1095bdd1
“Nhu <nhu.phamx@yahoo.com>
2022-08-30 02:35:28 +0000

    express-post-json

---
+++ express-post-json/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+let id = 1;
+const grades = [];
+
+app.use(express.json())
+
+app.get('/api/grades', (req, res) => {
+  res.json(grades);
+});
+
+app.post('/api/grades', (req, res) => {
+  // console.log(req.body)
+  grades.push({...req.body, id})
+  id++
+  res.send('success')
+})
+
+// const port = 5000
+// app.listen(port, () => console.log('lisenigaijgd'))
