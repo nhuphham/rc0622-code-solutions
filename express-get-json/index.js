7d15810099d31a6d0428f59f94253a5eb5d6b784
“Nhu <nhu.phamx@yahoo.com>
2022-08-30 01:52:38 +0000

    express-get-json

---
+++ express-get-json/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+};
+
+app.use('/api/grades', (req, res) => {
+  res.json(grades);
+});
+
+// const port = 5000;
+// app.listen(port, () => console.log('listeningggg'))
