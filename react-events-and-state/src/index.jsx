5d073835823171bb4c4f0bf7128fae60022c08ca
“Nhu <nhu.phamx@yahoo.com>
2022-09-01 18:46:49 +0000

    react-events-and-state

---
+++ react-events-and-state/src/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+class CustomButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { isClicked: false };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    this.setState(prevState => ({
+      isClicked: !prevState.isClicked
+    }));
+
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        {this.state.isClicked ? 'Thanks!' : 'Click Me!'}
+      </button>
+    );
+
+  }
+}
+
+const container = document.querySelector('#root');
+const root = ReactDOM.createRoot(container);
+
+root.render(<CustomButton />);
