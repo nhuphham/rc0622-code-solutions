e518b25dece36cdf085adaf6f4d71a792306477a
“Nhu <nhu.phamx@yahoo.com>
2022-09-08 23:12:04 +0000

    react-app-drawer-component

---
+++ react-app-drawer-component/src/index.jsx
@@ -0,0 +1,7 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import AppDrawer from './app-drawer';
+const element = <AppDrawer />;
+const root = ReactDOM.createRoot(document.querySelector('#root'));
+root.render(element);
